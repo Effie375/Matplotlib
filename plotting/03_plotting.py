ff15085e382cf3d7e2213810550ce1f47c5fa202
Efstratia Kazi <efoulakaz@hotmail.com>
2022-04-01 16:33:29 +0300

    Create 03_plotting.py

---
+++ plotting/03_plotting.py
@@ -0,0 +1,17 @@
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
